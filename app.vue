2a5c19a4cc38be5c8ebec34c206fe26afca368db
Your Name <you@example.com>
2022-04-30 11:39:36 +0500

    test

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>1</div>
+  <div>
+    <p v-for="item in stats" :key="item.id">
+      {{ stats }}
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { data: stats } = await useAsyncData("stats", () =>
+  $fetch("https://api.roastandbrew.coffee/api/v1/companies/")
+);
+</script>
