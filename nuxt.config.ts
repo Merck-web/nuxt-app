8b71baaa035c2d674e10885ab453d6b8c52e1c3f
Your Name <you@example.com>
2022-05-10 17:15:17 +0500

    gsap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,47 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  ssr: true,
+  app: {
+    head: {
+      title: "landing",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { hid: "description", name: "description", content: "" },
+        { name: "format-detection", content: "telephone=no" },
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
 
-})
+  css: [
+    "~/assets/css/index.scss",
+    // 'vue-slick-carousel/dist/vue-slick-carousel.css',
+  ],
+
+  plugins: [],
+
+  buildModules: [
+    // '@nuxtjs/tailwindcss',
+  ],
+
+  modules: [
+    // '@nuxtjs/axios'
+    // '@nuxtjs/pwa',
+  ],
+
+  build: {
+    transpile: [
+      // 'vue-carousel'
+    ],
+    loaders: {
+      vue: {
+        compilerOptions: {
+          // This option is highly recommended
+          preserveWhitespace: false,
+        },
+      },
+    },
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,143 @@
+<template>
+  <body>
+    <!-- <p v-for="item in stats" :key="item.id">
+      <span> {{ item.title }} </span>
+    </p> -->
+    <h1 class="header">Architecture <span>Loading...</span></h1>
+
+    <div class="gallery">
+      <div class="gallery__item"><img src="img/1.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/2.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/3.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/4.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/5.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/6.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/7.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/8.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/9.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/10.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/11.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/12.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/13.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/14.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/15.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/16.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/17.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/18.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/19.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/20.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/1.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/2.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/3.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/4.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/5.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/6.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/7.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/8.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/9.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/10.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/11.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/12.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/13.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/14.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/15.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/16.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/17.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/18.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/19.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/20.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/1.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/2.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/3.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/4.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/5.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/6.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/7.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/8.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/9.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/10.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/11.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/12.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/13.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/14.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/15.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/16.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/17.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/18.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/19.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/20.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/1.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/2.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/3.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/4.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/5.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/6.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/7.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/8.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/9.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/10.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/11.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/12.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/13.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/14.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/15.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/16.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/17.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/18.jpg" alt="Alt" /></div>
+      <div class="gallery__item"><img src="img/19.jpg" alt="Alt" /></div>
+    </div>
+  </body>
+</template>
+
+<script setup>
+const { data: stats } = await useAsyncData("stats", () =>
+  $fetch("https://jsonplaceholder.typicode.com/posts")
+);
+
+console.log(stats.value[1].title);
+</script>
+<script>
+export default {
+  methods: {
+    importGsap() {
+      let first = document.createElement("script");
+      first.setAttribute("src", "assets/js/gsap.min.js");
+      document.head.appendChild(first);
+    },
+    importDraggable() {
+      let second = document.createElement("script");
+      second.setAttribute("src", "assets/js/Draggable.min.js");
+      document.head.appendChild(second);
+    },
+    importInert() {
+      let third = document.createElement("script");
+      third.setAttribute("src", "assets/js/InertiaPlugin.min.js");
+      document.head.appendChild(third);
+    },
+    gras() {
+      setTimeout(function () {
+        document.body.classList.add("loaded");
+
+        if (window.matchMedia("(min-width: 992px)").matches) {
+          // If not mobile
+
+          Draggable.create(".gallery", {
+            bounds: "body",
+            inertia: true,
+          });
+        }
+      }, 200);
+    },
+  },
+  beforeMount() {
+    this.importGsap();
+    this.importDraggable();
+    this.importInert();
+  },
+  mounted() {
+    this.importDraggable();
+    this.importInert();
+    this.gras();
+  },
+};
+</script>
