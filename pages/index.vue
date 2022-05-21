eb9636afd2322e83e9248efb958c6b0c1a170b6f
sdjfnsd <alekseyt.ognevoy@mail.ru>
2022-05-21 10:19:32 +0500

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,30 @@
 <template>
-  <body>
-    <client-only>
-      <h1 class="header">Cats <span>Loading...</span></h1>
-    </client-only>
+  <client-only>
+    <h1 class="header">Cats <span>Loading...</span></h1>
     <div class="gallery__box">
       <div id="gallery" class="gallery">
         <div v-for="cat in stats" :key="cat.id" class="gallery__item">
-          <img :src="cat.url" :alt="cat.url" />
+          <img :src="cat.url" :alt="cat.url"/>
         </div>
       </div>
     </div>
-  </body>
+  </client-only>
+
 </template>
 
 <script setup>
-const { data: stats } = await useAsyncData("stats", () =>
-  $fetch("https://api.thecatapi.com/v1/images/search?limit=100", {
-    "x-api-key": "fe055ebb-1c00-4370-84ae-158fba71d480",
-  })
+const {data: stats} = await useAsyncData("stats", () =>
+    $fetch("https://api.thecatapi.com/v1/images/search?limit=100", {
+      "x-api-key": "fe055ebb-1c00-4370-84ae-158fba71d480",
+    })
 );
 
 // console.log(stats.value);
 </script>
 <script>
 // typical import
-import { gsap } from "gsap";
-import { Draggable } from 'gsap/all';
+import {gsap} from "gsap";
+import {Draggable} from 'gsap/all';
 
 if (process.client) {
   gsap.registerPlugin(Draggable);
@@ -34,7 +33,6 @@ export default {
   methods: {
     gras() {
       setTimeout(function () {
-        document.body.classList.add("loaded");
 
         if (window.matchMedia("(min-width: 992px)").matches) {
           // If not mobile
